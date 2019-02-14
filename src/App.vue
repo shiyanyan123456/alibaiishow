e2ac557b5e2b67cd662a2ddfebeda93f1ab925d8
shiyanyan <3081299758@qq.com>
2019-02-14 18:30:21 +0800

    登录-发送请求

--- src/App.vue
+++ src/App.vue
@@ -28,4 +28,7 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 } */
+#app{
+  height: 100%;
+}
 </style>
--- src/components/login.vue
+++ src/components/login.vue
@@ -1,32 +1,73 @@
 <template>
   <div class="login-wrap">
-    <el-form 
-    class="login-form"
-    label-position="top" label-width="80px" :model="formdata">
+    <el-form class="login-form" label-position="top" label-width="80px" :model="formdata">
       <h2>用户登录</h2>
-  <el-form-item label="用户名称">
-    <el-input v-model="formdata.username"></el-input>
-  </el-form-item>
-  <el-form-item label="密码">
-    <el-input v-model="formdata.password"></el-input>
-  </el-form-item>
-  <el-button type="primary" class="login-btn">登录</el-button>
-</el-form>
+      <el-form-item label="用户名">
+        <el-input v-model="formdata.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formdata.password"></el-input>
+      </el-form-item>
+      <el-button @click.prevent="handlelogin()" class="login-btn" type="primary">登录</el-button>
+    </el-form>
   </div>
-  </template>  
-<script>
+</template> 
+ <script>
 export default {
-  data(){
-    return{
-      formdata:{
-        username:"",
-        password:""
+   data() {
+    return {
+      formdata: {
+        username: "",
+        password: ""
       }
+    };
+  },
+  methods:{
+    handlelogin(){
+      // console.log(1111)
+      // console.log(this.formdata)
+
+      this.$http.post(`login`,this.formdata)
+      .then((res)=>{
+       console.log(res)
+       const{
+          data:{
+          data,
+          meta:{msg,status}
+        }
+       }=res
+      //  const{per}={per:"abc"}
+      if(status===200){
+        console.log('login----sucess---')
+      }else{
+        console.log('error---')
+      }
+      })
+      .catch(err=>{
+        console.log(err)
+      })
     }
   }
 };
 </script>
+
 <style>
+.login-wrap{
+  height: 100%;
+  background-color:#324152;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+}
+.login-form{
+  background-color:#fff;
+  width:400px;
+  border-radius:5px;
+  padding:30px;
+}
+.login-btn{
+  width: 100%;
+}
 </style>
 
 
