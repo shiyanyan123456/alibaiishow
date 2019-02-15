f55f742884a022538320f3d437f6e8aec37b8965
shiyanyan <3081299758@qq.com>
2019-02-15 22:58:11 +0800

    uers分支

--- src/components/users.vue
+++ src/components/users.vue
@@ -1,13 +1,170 @@
 <template>
-    <div>
-        users-----
-    </div>
+    <el-card class="box">
+        <!-- 面包屑 -->
+      <el-breadcrumb separator-class="el-icon-arrow-right">
+  <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+  <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+  <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+</el-breadcrumb>
+
+    <!-- 搜索框 -->
+    <!-- 让它整体在一行显示 -->
+    <el-row class="searchBox">
+        <el-col>
+            <el-input 
+            @clear="getAllUsers()"
+            clearable
+             class="searchInput" placeholder="请输入内容" v-model="query" >
+            <el-button 
+            @click="searchUser()"
+            slot="append" icon="el-icon-search"></el-button>
+            
+            </el-input>
+            <el-button type="primary" @click="showDiaAddUser()">添加用户</el-button>
+        </el-col>
+    </el-row>
+    
+    <!-- 表格 -->
+    <el-table :data="list" style="width:100%">
+      <el-table-column prop="id" label="#" width="80"></el-table-column>
+      <el-table-column prop="username" label="姓名" width="100"></el-table-column>
+      <el-table-column prop="email" label="邮箱" width="140"></el-table-column>
+      <el-table-column prop="mobile" label="电话" width="140"></el-table-column>
+     
+      <el-table-column  label="创建日期" width="200">
+          <template slot-scope="list">
+              {{list.row.create_time|fmtdate}}
+          </template>
+      </el-table-column>
+
+
+      <el-table-column  label="用户状态" width="120">
+        <template slot-scope="scope">
+            <el-switch v-model="scope.row.mg_state" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+        </template>
+      </el-table-column>
+
+    <el-table-column  label="操作" width="200">
+      <template slot-scope="scope">
+          <el-button type="primary" icon="el-icon-edit" circle size="mini" plain></el-button>
+          <el-button type="danger" icon="el-icon-delete" circle size="mini" plain></el-button>
+          <el-button type="success" icon="el-icon-check" circle size="mini" plain></el-button>
+      </template>
+    </el-table-column>
+    </el-table>
+
+    <!-- 分页 -->
+     <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="pagenum"
+      :page-sizes="[2,4,6,8]"
+      :page-size="2"
+      layout="total,sizes,prev, pager, next, jumper"
+      :total="total">
+    </el-pagination>
+    
+
+    <!-- 添加用户对话框 -->
+    <el-dialog title="收货地址" :visible.sync="dialogFormVisibleadd">
+      <el-form label-position="left" label-width="80px" :model="formdata">
+  <el-form-item label="用户名">
+    <el-input v-model="formdata.username"></el-input>
+  </el-form-item>
+  <el-form-item label="密码">
+    <el-input v-model="formdata.password"></el-input>
+  </el-form-item>
+  <el-form-item label="邮箱">
+    <el-input v-model="formdata.email"></el-input>
+  </el-form-item>
+  <el-form-item label="电话">
+    <el-input v-model="formdata.mobile"></el-input>
+  </el-form-item>
+</el-form>
+  <div slot="footer" class="dialog-footer">
+    <el-button @click="dialogFormVisibleadd = false">取 消</el-button>
+    <el-button type="primary" @click="dialogFormVisibleadd = false">确 定</el-button>
+  </div>
+</el-dialog>
+</el-card>
 </template>
 <script>
 export default {
+  data() {
+    return {
+      //搜索关键字
+      query: "",
+      pagenum: 1,
+      pagesize: 2,
+      total: -1,
+      list: [],
+      dialogFormVisibleadd: false,
+      formdata:{
+        username:"",
+        password:"",
+        email:"",
+        mobile:""
+      }
+    };
+  },
+  created() {
+    this.getTableData();
+  },
+  methods: {
+    //显示对话框
+      showDiaAddUser() {
+      this.dialogFormVisibleadd = true;
+    },
 
-}
+    //清空时获取所有用户
+    getAllUsers() {
+      this.getTableData();
+    },
+
+    // 搜索
+    searchUser() {
+      this.pagenum = 1;
+      this.getTableData();
+    },
+
+    // 分页
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`);
+      this.pagenum = 1;
+      this.pagesize = val;
+      this.getTableData();
+    },
+    // 当前1页 -> 点击2页 -> 获取第二页数据
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`);
+      this.pagenum = val;
+      this.getTableData();
+    },
+    async getTableData() {
+      const AUTH_TOKEN = localStorage.getItem("token");
+      // console.log(AUTH_TOKEN);
+      this.$http.defaults.headers.common["Authorization"] = AUTH_TOKEN;
+      const res = await this.$http.get(
+        `users?query=${this.query}&pagenum=${this.pagenum}&pagesize=${
+          this.pagesize
+        }`
+      );
+      console.log(res);
+      const { data, meta: { status, msg } } = res.data;
+      if (status === 200) {
+        this.total = data.total;
+        this.list = data.users;
+        console.log(this.list);
+      }
+    }
+  }
+};
 </script>
 <style>
-
+.searchBox {
+  margin-top: 30px;
+}
+.searchInput {
+  width: 400px;
+}
 </style>
