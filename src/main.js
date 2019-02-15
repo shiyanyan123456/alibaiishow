87a71a5e43686458646b8d387cf17002449d962b
shiyanyan <3081299758@qq.com>
2019-02-15 23:10:49 +0800

    users分支日期修改

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import ElementUI from 'element-ui'
+import ElementUI from 'element-ui' 
+import moment from 'moment'
 import 'element-ui/lib/theme-chalk/index.css'
 import '@/assets/css/base.css'
 import App from './App'
@@ -17,6 +18,10 @@ Vue.prototype.$http = axios
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
+
+Vue.filter('fmtdate', (v) => {
+  return moment(v).format('YYYY-MM-DD')
+});
 
 /* eslint-disable no-new */
 new Vue({
